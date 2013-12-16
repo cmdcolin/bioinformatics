0a3adde475b599185632cd4aa853747c98907765
Colin D <colin.diesh@gmail.com>
2013-12-15 22:25:47 -0600

    Crazy additions to add mutation distance-doubt it will work

--- ch3/c1/motifenum.pl
+++ ch3/c1/motifenum.pl
@@ -1,8 +1,10 @@
 use strict;
 use warnings;
+#libs
 use Data::Dumper;
 use Text::Levenshtein qw(distance);
 
+#load input files
 my $header=<>;
 my ($k,$d)=split(' ',$header);
 my @dna;
@@ -13,12 +15,17 @@ while(my $line=<>) {
 
 #generate all kmers from input
 my %kmers_input;
-foreach my $string (@dna) {
+my $iter=0;
+for(my $iter=0;$iter<scalar @dna;$iter++) {
+	my $string=$dna[$iter];
 	for(my $i=0;$i<length($string)-$k;$i++) {
 		my $x=substr $string,$i,$k;
-		$kmers_input{$x}++;
+		push($kmers_input{$x},$iter);
 	}
 }
+
+#generate all possible kmers 
+#http://www.bioperl.org/wiki/Getting_all_k-mer_combinations_of_residues
 my @bases = ('A','C','G','T');
 my @words = @bases;
 for my $i (1..$k-1)
@@ -35,17 +42,25 @@ for my $i (1..$k-1)
 	undef @words;
 	@words = @newwords;
 }
-foreach my $w (@words)
-{
-	print "$w\n";
-}
 
-
+#motifenumerate as described on website
 sub motif_enum {
 	my $kmers_ref=shift;
 	foreach my $kmer (keys %{$kmers_ref}) {
-		foreach my $kmut (gen_mut($kmer,$d)) {
-			
+		my $mut_ref=gen_mut($kmer,$d);
+		foreach my $kmut (@{$mut_ref}) {
+			foreach $origkmer (keys %{$kmer_ref}) {
+				if(distance($kmut,$origkmer)<=$d) {
+					my %listgather;
+					my @dnalist_t=(${$kmer_ref}){$origkmer};
+					foreach my $dnaitem (@dnalist) {
+						$listgather{$dnaitem}++;
+					}
+					if(scalar keys %listgather==scalar @dna) {
+						print $kmut;
+					}
+				}
+			}
 		}
 	}	
 }
@@ -54,6 +69,13 @@ sub motif_enum {
 
 sub gen_mut {
 	my ($kmer, $d)=@_;
+	my @ret;
+	for my $key (@words) {
+		if(distance($key,$kmer)<$d) {
+			push(@ret,$key);
+		}
+	}
+	return \@ret;
 }
 
 
