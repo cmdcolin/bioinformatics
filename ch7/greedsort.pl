5cc7663e774c1c3c4e528c7596f2a89940f2ef33
Colin D <colin.diesh@gmail.com>
2014-01-27 23:44:55 -0600

    Picture perfect?

--- ch7/greedsort.pl
+++ ch7/greedsort.pl
@@ -1,13 +1,11 @@
 use strict;
 use Data::Dumper;
+my $verbose=0;
 my $line=<>;
-print $line;
 $line=~s/\((.*)\)/$1/;
-print $line;
 my @p=split(' ',$line);
-print Dumper \@p;
 my $d=greedy_sort(\@p);
-print "$d\n";
+print "approxReversalDistance\t$d\n";
 
 #test k_reversal
 #my @ktest=0..15;
@@ -19,17 +17,19 @@ print "$d\n";
 
 sub greedy_sort {
 	my $ref_p=shift;
-	my $d=0;	
+	my $d=0;
+	#print_rev($ref_p);
 	for(my $i=0;$i<0+@{$ref_p};$i++) {
 		my $k=@{$ref_p}[$i];
-		print "k sort (i,k,d)\t$i\t$k\t$d\n";
+		print "k sort (i,k,d)\t$i\t$k\t$d\n" if $verbose;
 		if(abs($k)!=$i+1) {
 			my @temp=k_reversal($ref_p,$i);
 			$ref_p=\@temp;
 			$d+=1;	
-			print_rev( $ref_p);
+			print_rev($ref_p);
 		}
-		elsif(abs($k)!=$k) {
+		my $k=@{$ref_p}[$i];
+		if(abs($k)!=$k) {
 			@{$ref_p}[$i]=@{$ref_p}[$i]*-1;
 			$d+=1;
 			print_rev($ref_p);
@@ -45,9 +45,9 @@ sub k_reversal {
 	my $l=0+@{$ref_p};#len
 	for(my $i=$s;$i<0+$l;$i++) {
 		my $k=@{$ref_p}[$i];
-		print "k_reversal (k,s) $k\t$s\n";
+		print "k_reversal (k,s) $k\t$s\n" if $verbose;
 		if(abs($k)==$s+1) {
-			print "$s,$i,$l\n";
+			print "$s,$i,$l\n"  if $verbose;
 			my @temp=reverse(@{$ref_p}[$s..$i]);
 			foreach my $x (@temp) { $x = $x * -1; }	
 			return @{$ref_p}[0..$s-1],@temp,@{$ref_p}[$i+1..$l-1];
@@ -61,7 +61,9 @@ sub print_rev {
 	my $ref_p=shift;
 	print "(";
 	for (my $i=0;$i<0+@{$ref_p};$i++) {
-		print @{$ref_p}[$i]." ";
+		if(@{$ref_p}[$i]>0) {print "+".int(@{$ref_p}[$i]);}
+		else{ print @{$ref_p}[$i];}
+		if($i!=0+@{$ref_p}-1){print " "};
 	}
 	print ")\n";
 }
